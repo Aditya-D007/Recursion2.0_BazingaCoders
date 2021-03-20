f1bb6a0c2c54d57090fc6c4925e8574a816ef9d6
Harsh Gandhi <18harshgandhi@gmail.com>
2021-03-20 16:18:45 +0530

    commit

--- hospital_management_backend/src/route/hospitalRoute.js
+++ hospital_management_backend/src/route/hospitalRoute.js
@@ -2,5 +2,8 @@ const express = require("express");
 const hospitalControllers = require("../controllers/hospitalControllers");
 const router = new express.Router();
 
+router.post('/register/hospital',hospitalControllers.registerhospital)
+
+router.post('/login/hospital',hospitalControllers.loginhospital)
 
 module.exports = router;
